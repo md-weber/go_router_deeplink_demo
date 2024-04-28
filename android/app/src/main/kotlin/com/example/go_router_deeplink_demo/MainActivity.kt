ab1ef4db05f24bee776d4df9a98b3567b2848d7b
Max Weber <weber.max.1988@gmail.com>
2024-04-28 21:33:25 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/go_router_deeplink_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.flutterexp.go_router_deeplink_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final routerConfig = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(title: const Text('Home Screen')),
+      ),
+      routes: [
+        GoRoute(
+          path: 'details',
+          builder: (_, __) => Scaffold(
+            appBar: AppBar(title: const Text('Details Screen')),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
